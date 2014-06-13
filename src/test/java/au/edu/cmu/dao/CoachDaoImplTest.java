e46a1dd11957a77638a9cec92851995fef1109eb
Celine Patag <chibeepatag@users.noreply.github.com>
2014-06-13 22:45:45 +0930

    version fix

--- src/test/java/au/edu/cmu/dao/CoachDaoImplTest.java
+++ src/test/java/au/edu/cmu/dao/CoachDaoImplTest.java
@@ -1,12 +1,15 @@
 package au.edu.cmu.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import au.edu.cmu.model.Coach;
 
-@ContextConfiguration("")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-dao-config.xml")
 public class CoachDaoImplTest {
 	
 	@Autowired
