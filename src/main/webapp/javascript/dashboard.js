0e47f290925d5446a460834e38ef9c801c5e77ed
Celine Patag <chibeepatag@users.noreply.github.com>
2014-07-15 21:49:54 +0930

    map now visible

--- src/main/webapp/javascript/dashboard.js
+++ src/main/webapp/javascript/dashboard.js
@@ -2,7 +2,7 @@
  * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
  * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
  */
-$( document ).on( "pageinit", "#map-page", function() {
+$( document ).on( "pageinit", "#xyz", function() {
     var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
     if ( navigator.geolocation ) {
         function success(pos) {
