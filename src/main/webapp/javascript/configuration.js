ce8931da3ae346e490e874560afc1eb864bec2a8
Richard Tiu <richardshannon.tiu@gmail.com>
2014-07-28 20:21:55 +0930

    fixed empty new riders, fixed edit new riders, fixed duplicate jersey in race

--- src/main/webapp/javascript/configuration.js
+++ src/main/webapp/javascript/configuration.js
@@ -24,6 +24,13 @@ function selectRow(){
 
 function addRider(){	
 	var riderId = $("#riderIdInput").val();
+	var firstName = $("#firstNameInput").val();
+	var lastName = $("#lastNameInput").val();
+	var nickname = $("#nicknameInput").val();
+	var phone = $("#phoneInput").val();
+	var jersey = $("#jerseyNoInput").val();
+	
+	
 	if(riderId > 0){		
 		$.ajax({
 			url: "editRider",
@@ -32,7 +39,7 @@ function addRider(){
 			type: "POST",
 			success: updateEditedRider
 		});
-	}else{
+	}else if (firstName !== "" && lastName !== "" && nickname !== "" && phone !== "" && jersey !== ""){
 		$.ajax({
 			url: "addRider",
 			data: $("#newRiderForm").serialize(),
@@ -40,7 +47,7 @@ function addRider(){
 			type: "POST",
 			success: appendNewRider
 		});
-	}	
+	} 	
 }
 
 function selectEditRider(){
@@ -70,6 +77,7 @@ function appendNewRider(data){
 	$(".riderTable").append(lastRowClone);
 	$(lastRowClone).removeClass("selectedRow");
 	$(lastRowClone).click(selectRow);
+	$(lastRowClone).dblclick(selectEditRider);
 }
 
 function updateEditedRider(data){
@@ -105,10 +113,25 @@ function createRace(){
 	var raceName = $("#raceName").val();
 		
 	if(raceName){
-		var riderIds = $(".selectedRow").find(".rider_id");		
-		if(riderIds.length > 0){
+		var riderIds = $(".selectedRow").find(".rider_id");
+		var riderJerseys = $(".selectedRow").find(".riderJersey");
+		
+		
+		var sameJersey= false;
+		for(var x=0; x<riderJerseys.length;x++){
+			for (var y=x+1; y<riderJerseys.length; y++){
+			  if (riderJerseys[x].innerText == riderJerseys[y].innerText){
+			  	sameJersey= true;
+			  }
+			}
+		} 
+		if (sameJersey == true){
+			$(".errorPopup").text("Jersey number duplicate");
+			$("#errorButton").click();
+		
+		}else if(riderIds.length > 0){
 			var selectedRows = $(riderIds).map(function(){return $(this).text()}).get().join(",");
-			
+	
 			$.ajax({
 				url: "createRace", 
 				data: {"raceName":raceName, "ids":selectedRows},
@@ -121,7 +144,7 @@ function createRace(){
 			$(".errorPopup").text("You must select at least one rider.");
 			$("#errorButton").click();
 		}
-				
+	
 	}else{
 		$(".errorPopup").text("You must enter a race name");
 		$("#errorButton").click();
