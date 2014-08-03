83cb8ebe65403707b3b09b70dad46d9d06be1b2b
Celine Patag <chibeepatag@users.noreply.github.com>
2014-08-03 12:44:41 +0930

    added received template

--- src/main/webapp/javascript/dashboard.js
+++ src/main/webapp/javascript/dashboard.js
@@ -5,7 +5,7 @@
 var markers = {};
 var interval;
 var map;
-var messageTemplateMap={"left":"Left", "right":"Right", "breakAway":"Break away", "timeGap":"Time Gap", "safety":"Safety"};
+var messageTemplateMap={"left":"Left", "right":"Right", "breakAway":"Break away", "timeGap":"Time Gap", "safety":"Safety", "received":"Received"};
 var startTime;
 
 $( document ).on( "pageinit", "#page", function() {
