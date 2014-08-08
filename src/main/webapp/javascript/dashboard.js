71bafdf2f9df99b6bcd5512bc3ddd72ad507ea23
Celine Patag <chibeepatag@users.noreply.github.com>
2014-08-08 12:53:15 +0930

    Uncommented out racedetails

--- src/main/webapp/javascript/dashboard.js
+++ src/main/webapp/javascript/dashboard.js
@@ -81,7 +81,7 @@ $(document).ready(function(){
 		$("#startRace").addClass("hiddenField");
 		$("#endRace").removeClass("hiddenField");
 		
-		//$("#raceDuration").removeClass("hiddenField");
+		$("#raceDuration").removeClass("hiddenField");
 	}else{
 		$("#endRace").addClass("hiddenField");
 		$("#startRace").removeClass("hiddenField");
@@ -265,7 +265,7 @@ function startRace(){
 			console.log('Start: ' + data.race_start);
 			startTime = new Date(data.race_start);
 			$("#raceStartTime").text(startTime);			
-			//$("#raceDuration").removeClass("hiddenField");
+			$("#raceDuration").removeClass("hiddenField");
 			$("#configBtn").addClass("ui-disabled");
 		}
 	});
